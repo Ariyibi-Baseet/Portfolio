31bbb1094899ef546b90e3f1285b4d81fc6c6e53
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-02-01 14:01:08 +0100

    fix tailwind installation error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   // path to all of my template
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js,css}"],
   theme: {
     extend: {},
-  },
-  plugins: [],
+  }
 }
