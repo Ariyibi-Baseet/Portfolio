9012c73501c949ec205f39385641e68db4b0760e
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-02-15 22:24:05 +0100

    add color scheme and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
+  // implementing darkmode
+  // darkMode: 'class',
+
   // path to all of my template
-  content: ["./public/**/*.{html,js,css}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deep_blue : "#041E42",
+        maya_blue : "#73C2FB",
+      },
+    },
+    fontFamily: {
+      'Montserrat-Alternates' : ['Montserrat Alternates', 'sans-serif'],
+      'Montserrat' : ['Montserrat', 'sans-serif'],
+      'Poppins' : ['Poppins', 'sana-serif']
+    }
   }
 }
+
+
+
+
+
