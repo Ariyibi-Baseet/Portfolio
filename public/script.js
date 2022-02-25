803a2dd802cb7a70c7c378f5b23ffc1a75bd768f
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-02-25 15:28:46 +0100

    navbar toggling

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const NavLinks = document.getElementById('navLinks');
 
 Icon.addEventListener('click',() => {
 NavLinks.classList.toggle("max-sm:hidden");
-NavLinks.classList.add('max-sm:block','max-sm:pl-9','max-sm:absolute','max-sm:left-0','max-sm:w-full','max-sm:mt-64','max-sm:bg-deep_blue');
+NavLinks.classList.add('max-sm:flex','max-sm:flex-col','max-sm:text-left','max-sm:absolute','max-sm:right-0','max-sm:top-0','max-sm:w-full','max-sm:min-h-screen','max-sm:bg-deep_blue','max-sm:border','max-sm:items-center' ,'max-sm:justify-center');
 
 //     if(NavLinks.style.display == "flex")
 // {
