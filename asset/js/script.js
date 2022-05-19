601e2191cfd1c4d578ecda71aa868545fa91136d
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-05-19 11:05:18 +0100

    create general script for the page and also include bootstrap popper.min.js file

---
+++ asset/js/script.js
@@ -0,0 +1,13 @@
+const preloader = document.querySelector('#preloader');
+
+// window.addEventListener('load', showPreloader);
+
+// function showPreloader()
+// {
+//     preloader.style.display = flex;
+// }
+
+
+document.onload = () => {
+    preloader.style.display = block;
+};
